e7e0a0918a7f74d686ac8d9240b9c81968659510
JiFeng <2996710293@qq.com>
2020-08-05 11:31:20 +0800

    创建图形逼近

---
+++ 临时工具/图像补全/render.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+sources=[(4,5.5),(5,5),(6,4),(7,2)]
+print(sources)
+xx=[]
+yy=[]
+for point in sources:
+    xx.append(point[0])
+    yy.append(point[1])
+
+def plotFcn(k,b):
+    x=[i for i in range(10)]
+    y=[k*i+b for i in x]
+    plt.plot(x,y,'--')
+def getKB(p0,p1):
+    k=(p1[1]-p0[1])/(p1[0]-p0[0])
+    b=p0[1]-p0[0]*k
+    print(k,b)
+    return [k,b]
+Fcn=[]
+
+for num in range(len(sources)-1):
+    Fcn.append(getKB(sources[num],sources[num+1]))
+    plotFcn(Fcn[num][0],Fcn[num][1])
+# Fcn.append(getKB(sources[0],sources[1]))
+# plotFcn(Fcn[0][0],Fcn[0][1])
+
+# Fcn.append(getKB(sources[1],sources[2]))
+# plotFcn(Fcn[1][0],Fcn[1][1])
+def shineKB(Fcn):
+    shine=Fcn[1][0]/Fcn[0][0]
+    newS=Fcn[0][0]/shine
+    return newS
+
+
+shineKB(Fcn)
+plt.plot(xx,yy)
+plt.axis([0,10,0,10])
+plt.show()
