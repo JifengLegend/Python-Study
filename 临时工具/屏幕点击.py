3183465db7081d0ae304243fab13cc2372927f7d
JifengLegend <2996710293@qq.com>
2020-08-14 22:33:50 +0800

    提交pyautog是、

--- 临时工具/屏幕点击.py
+++ 临时工具/屏幕点击.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 
 def getPosition():
-    print("请在五秒后，把鼠标放到预点击位置\n")
+    print("--请在五秒后，把鼠标放到预点击位置:")
     for s in range(5):
 
         print(5-s)
@@ -12,15 +12,15 @@ def getPosition():
         # Get and print the mouse coordinates.
             x, y = pyautogui.position()
             X,Y=str(x).rjust(4),str(y).rjust(4)
-            positionStr = f'鼠标坐标点（X,Y）为：{X},{Y}'
+            positionStr = f'--鼠标位置获取成功！目标点为：{X},{Y}'
             pix = pyautogui.screenshot().getpixel((x, y)) # 获取鼠标所在屏幕点的RGB颜色
             positionStr += ' RGB:(' + str(pix[0]).rjust(3) + ',' + str(pix[1]).rjust(3) + ',' + str(pix[2]).rjust(
                 3) + ')'
             print(positionStr)
             time.sleep(0.5) # 停顿时间
     except:
-        print('获取鼠标位置失败')
-    return X,Y
+        print('!获取鼠标位置失败!')
+    return x,y
 
 # 获取鼠标位置
 
@@ -28,7 +28,7 @@ def getPosition():
 # pyautogui.click(638,31)
 
 if __name__ == "__main__":
-    print('start')
+    print('start Program')
     startJudge=1
     while startJudge==1:
         setTimeRaw=input('--请输入要设定时间（xx.xx分钟后）：\n')
@@ -44,9 +44,19 @@ if __name__ == "__main__":
             
     print(setTime)
     x,y=getPosition()
-    time.sleep(setTime)
-    print('111')
-    pyautogui.click(int(x),int(y))
+    print(f"将在{setTime}s后执行")
+    if setTime>=5:
+        time.sleep(setTime-5)
+        for eachsecond in range(5):
+            print(f'left time :: {5-eachsecond}s')
+            time.sleep(1)
+    else:
+        for eachsecond in range(setTime):
+            print(f'left time :: {setTime-eachsecond}s')
+            time.sleep(1)
+    pyautogui.click(x,y)
+    print('--完成点击')
+
 
     
             
