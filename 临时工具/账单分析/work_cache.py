7de6a667ccdade02cd9732fb9d7fe8a334af163e
JiFeng <2996710293@qq.com>
2020-10-20 01:01:14 +0800

    优化账单分析逻辑

--- 临时工具/账单分析/work_cache.py
+++ 临时工具/账单分析/work_cache.py
@@ -35,7 +35,7 @@ class PayMess:
         self.k=1 if self.k=="收入" else -1
         self.money= float(self.l[payMoney][1:])*self.k
     def calc(self):
-        # print(self.money,self.l[6],self.l[payObj],self.l[payTime])
+        # print(self.money,self.l[6],self.l[payObj])
         return self.money
 class ConList():
     def __init__(self,payList,text):
@@ -45,24 +45,44 @@ class ConList():
             self.cache=PayMess(each)
             self.money+=self.cache.calc()
         judge='+' if self.money>0 else ''
-        print(f'{text}的收支计算结果为：\n\t{judge}{self.money:.2f}')
+        print(f'{text}共：\n\t{judge}{self.get()}')
         
     def get(self):
-        return self.money
+        return f2f(self.money)
 
+def f2f(num):
+    cache=f'{num:.2f}'
+    return float(cache)
+def moneyAdd(*l):
+    money=0
 
+    for each in l:
+        money+=each.money
+    return f2f(money)
 def inMoney(l):
     money=0
     for each in l:
         money+=float(each[payMoney][1:])
-    print(f'从零钱里面转入零钱通共：\n\t{money}')
-    return money
+    print(f'从零钱里面转入零钱通共：\n\t{f2f(money)}')
+    return f2f(money)
+def redMoney(l):
+    money=0
+    for each in l:
+        money-=float(each[payMoney][1:])
+    print(f'群发红包共：\n\t{f2f(money)}')
+    return f2f(money)
+def redBackMoney(l):
+    money=0
+    for each in l:
+        money+=float(re.search(r'\d+\.\d+',each[payState]).group())
+    print(f'从红包中退款共：\n\t{f2f(money)}')
+    return f2f(money)
 
 if __name__ == "__main__":
     chRec()
     # fileName=chFile()
 
-    with open('微信支付账单(20200917-20201017).csv','r',encoding='utf-8')as f:
+    with open('[偈妮]账单(20200917-20201017).csv','r',encoding='utf-8')as f:
         reader=csv.reader(f)
         sheet=list(reader)
         name=re.search(r'\[.*\]',sheet[1][0]).group()
@@ -71,7 +91,10 @@ if __name__ == "__main__":
         listDef=[]
         listSelf=[]
         listIn=[]
-
+        listAdd=[]
+        listDec=[]
+        listRed=[]
+        listRedBack=[]
         for row in sheet:
             if (len(row)==11) and (len(row[0])!=4):
                 # print(row)
@@ -81,13 +104,31 @@ if __name__ == "__main__":
                     listDef.append(row)
                 if (row[4]=='/'):
                     listIn.append(row)
+                if (row[2]=='瀮风') and (row[payMoney]=='¥200.00'):
+                    listAdd.append(row)
+                if(row[2]=="发出群红包") :
+                    listRed.append(row)
+                    if (row[payState][0:2]=='已退'):
+                        listRedBack.append(row)
+                if (row[payMoney]=='¥80.00')or (row[payMoney]=='¥40.00'):
+                    listDec.append(row)
         # print(listSelf)
         print(f'账单归属人：{name}')
         def_money=ConList(listDef,'零钱(默认)')
         other_money=ConList(listSelf,'其它')
-        in_money=inMoney(listIn)
+        # in_money=inMoney(listIn)
 
-        print(f'零钱通最终结余：\n\t{other_money.money+in_money:.2f}')
+        money_In=ConList(listAdd,'瀮风转入')
+        money_Dec=ConList(listDec,"转账支出")
+        
+        red_Money=ConList(listRed,'群发红包')
+        back_Money=redBackMoney(listRedBack)
+
+        print(f'{moneyAdd(money_In,money_Dec,red_Money)+back_Money}')
+
+
+
+        # print(f'零钱通最终结余：\n\t{other_money.money+in_money:.2f}')
         
 
 
